44155461c5fe8a89f3042467c2d5a874a4cf1b29
Marc <mdd_13@hotmail.com>
2012-12-24 14:23:02 +0100

    Added JavaDoc. Fixed SameName function to compare in lower case.

--- src/graph/components/NodeNames.java
+++ src/graph/components/NodeNames.java
@@ -2,26 +2,46 @@
  * File: NodeNames.java
  * Created on 07/12/2012 by Marc
  * 
+ * @author Marc
  * Class that represents a Node Name
  */
 package graph.components;
 
 public class NodeNames {
-    
+    /**
+     * The name
+     */
     private String Name;
     
+    /**
+     * Constructor.
+     * @param n is the name for that node.
+     */
     public NodeNames(String n) {
 	Name = n;
     }
     
+    /**
+     * Getter of the name.
+     * @return The name of that NodeName.
+     */
     public String getName() {
 	return Name;
     }
     
+    /**
+     * Function to compare if a name is the same.
+     * @param n name to compare.
+     * @return True if the name is the same.
+     */
     public boolean SameName(String n) {
-	return Name.equals(n);
+	return Name.toLowerCase().equals(n.toLowerCase());
     }
     
+    /**
+     * Setter of the name.
+     * @param n is the new name to set.
+     */
     public void setName(String n) {
 	Name = n;
     }
