de62698baf2ff4503334577daf75bccab7e9db94
Marc <mdd_13@hotmail.com>
2012-12-26 14:54:22 +0100

    Added a new text field to input the source and the destination.

--- src/window/Window.java
+++ src/window/Window.java
@@ -1,27 +1,34 @@
 package window;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JTextField;
-import java.awt.BorderLayout;
-import javax.swing.JButton;
-import javax.swing.JTextPane;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.util.ArrayList;
-
 import graph.components.Graph;
 import graph.components.Node;
 import graph.parser.Parser;
 
+import java.awt.BorderLayout;
+import java.awt.ComponentOrientation;
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
+
 public class Window {
 
     private JFrame frame;
-    private JTextField textField;
     private Graph g;
     private ArrayList<Node> nodes;
     private Parser p;
+    private JTextField textField;
+    private JTextField textField_1;
 
     /**
      * Launch the application.
@@ -56,10 +63,6 @@ public class Window {
 	
 	p = new Parser();
 	
-	textField = new JTextField();
-	frame.getContentPane().add(textField, BorderLayout.NORTH);
-	textField.setColumns(10);
-	
 	JButton btnParse = new JButton("Parse");
 	btnParse.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent arg0) {
@@ -86,6 +89,39 @@ public class Window {
 		}
 	});
 	frame.getContentPane().add(btnSearch, BorderLayout.EAST);
+	
+	JPanel panel = new JPanel();
+	frame.getContentPane().add(panel, BorderLayout.NORTH);
+	GridBagLayout gbl_panel = new GridBagLayout();
+	gbl_panel.columnWidths = new int[]{217, 216, 0};
+	gbl_panel.rowHeights = new int[]{20, 0};
+	gbl_panel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+	gbl_panel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+	panel.setLayout(gbl_panel);
+	
+	textField = new JTextField();
+	textField.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+	textField.setToolTipText("Origen");
+	textField.setHorizontalAlignment(SwingConstants.CENTER);
+	GridBagConstraints gbc_textField = new GridBagConstraints();
+	gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+	gbc_textField.anchor = GridBagConstraints.NORTH;
+	gbc_textField.insets = new Insets(0, 0, 0, 5);
+	gbc_textField.gridx = 0;
+	gbc_textField.gridy = 0;
+	panel.add(textField, gbc_textField);
+	textField.setColumns(10);
+	
+	textField_1 = new JTextField();
+	textField_1.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+	textField_1.setHorizontalAlignment(SwingConstants.LEFT);
+	textField_1.setToolTipText("Desti");
+	GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+	gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+	gbc_textField_1.gridx = 1;
+	gbc_textField_1.gridy = 0;
+	panel.add(textField_1, gbc_textField_1);
+	textField_1.setColumns(10);
     }
 
 }
