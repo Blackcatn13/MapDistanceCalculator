dafd947ba13c918f0e54fa55f6c90aae19bcd0d8
Marc <mdd_13@hotmail.com>
2012-12-27 11:04:28 +0100

    Changed the value of the heuristic.

--- src/algorithm/Heuristic1.java
+++ src/algorithm/Heuristic1.java
@@ -5,6 +5,7 @@ import graph.components.Node;
 
 public class Heuristic1 extends Heuristic{
 
+    enum names {n1, n2, n3, n4, n5, n6};
     @Override
     public void init(Graph g) {
 	this.setGraph(g);
@@ -12,7 +13,10 @@ public class Heuristic1 extends Heuristic{
 
     @Override
     public float Calculate(Node s, Node d) {
-	return 0;
+	if(s.costTo(d) != -1) {
+	    return s.costTo(d);
+	}
+	return 5;
     }
 
 }
