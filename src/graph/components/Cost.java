900ece1ab3321dffcb29c6d61c85f16186dc67ec
Marc <mdd_13@hotmail.com>
2012-12-28 14:54:58 +0100

    Added cost from transports. Added getter's and setter's for the new cost system.

--- src/graph/components/Cost.java
+++ src/graph/components/Cost.java
@@ -8,12 +8,14 @@ package graph.components;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.EnumMap;
 import java.util.Iterator;
 
 public class Cost {
 
     private ArrayList<Date> schedule = new ArrayList<Date>();
     private ArrayList<Float> cost = new ArrayList<Float>();
+    private EnumMap<Transports, Float> costs = new EnumMap<Transports, Float>(Transports.class);
     private float defaultCost;
     private boolean haveSchedule = false;
     
@@ -49,5 +51,29 @@ public class Cost {
     
     public float getCost() {
 	return defaultCost;
+    }
+    
+    public void setBusC(float c) {
+	costs.put(Transports.BUS, c);
+    }
+    
+    public void setSubWC(float c) {
+	costs.put(Transports.SUBWAY, c);
+    }
+    
+    public void setWalkC(float c) {
+	costs.put(Transports.WALK, c);
+    }
+    
+    public float getBusC() {
+	return costs.get(Transports.BUS);
+    }
+    
+    public float getSubWC() {
+	return costs.get(Transports.SUBWAY);
+    }
+    
+    public float getWalkC() {
+	return costs.get(Transports.WALK);
     }
 }
