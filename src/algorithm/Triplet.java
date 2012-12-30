91d37ba19830c7af48a61eff36f2891df95f78c1
Marc <mdd_13@hotmail.com>
2012-12-30 12:50:58 +0100

    Added comments to the Triplet class.

--- src/algorithm/Triplet.java
+++ src/algorithm/Triplet.java
@@ -16,34 +16,69 @@ import java.util.ArrayList;
 
 public class Triplet implements Comparable<Object>{
 
+    /**
+     * Variable to hold the f(x) value.
+     */
     private float Fx;
+    /**
+     * Variable to hold the g(x) value.
+     */
     private float Gx;
+    /**
+     * Variable to hold the path.
+     */
     private ArrayList<Node> path;
     
+    /**
+     * Default constructor of the class.
+     */
     public Triplet() {
 	Fx = 0;
 	Gx = 0;
 	path = new ArrayList<Node>();
     }
     
+    /**
+     * Constructor of the class that take all the arguments to save it.
+     * @param f value of the f(x).
+     * @param g value of the g(x).
+     * @param p path.
+     */
     public Triplet(float f, float g, ArrayList<Node> p) {
 	Fx = f;
 	Gx = g;
 	path = p;
     }
     
+    /**
+     * Getter of the f(x) value.
+     * @return the f(x) value.
+     */
     public float getFx() {
 	return Fx;
     }
     
+    /**
+     * Getter of the g(x) value.
+     * @return the g(x) value.
+     */
     public float getGx() {
 	return Gx;
     }
     
+    /**
+     * Getter of the path.
+     * @return the path.
+     */
     public ArrayList<Node> getPath(){
 	return path;
     }
     
+    /**
+     * Function to now if the path is the same.
+     * @param a triplet to compare.
+     * @return true if the path is the same.
+     */
     public boolean EqualPath(Triplet a) {
 	if(this.path == a.path) {
 	    return true;
@@ -51,10 +86,17 @@ public class Triplet implements Comparable<Object>{
 	return false;
     }
     
+    /**
+     * Function to get the last node of the path.
+     * @return the last node of the path.
+     */
     public Node getFirst() {
 	return path.get(path.size() - 1);
     }
 
+    /**
+     * Function to compare two elements to add it in order.
+     */
     @Override
     public int compareTo(Object a) {
 	Triplet a_ = (Triplet)a;
