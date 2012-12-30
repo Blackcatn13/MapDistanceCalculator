0262f7b00afadc62a3b87f3b0be658d7b621fc5d
Marc <mdd_13@hotmail.com>
2012-12-30 13:17:28 +0100

    Added comments to the Graph class.

--- src/graph/components/Graph.java
+++ src/graph/components/Graph.java
@@ -11,16 +11,32 @@ import java.util.ArrayList;
 
 public class Graph {
 
+    /**
+     * Variable that holds all the nodes of the graph.
+     */
     private ArrayList<Node> nodes;
     
+    /**
+     * Default constructor.
+     */
     public Graph() {
 	nodes = new ArrayList<Node>();
     }
     
+    /**
+     * Function to add a new node.
+     * @param n new node to add.
+     */
     public void addNode(Node n) {
 	nodes.add(n);
     }
     
+    /**
+     * Function to get the neighbors of a node.
+     * @param name of the node to get the neighbors.
+     * @param alias of the node to get the neighbors.
+     * @return An ArrayList with all the neighbors of the node.
+     */
     public ArrayList<Node> getNeighbors(String name, String alias) {
 	int node = 0;
 	for(int i = 0; i < nodes.size(); i++) {
@@ -30,9 +46,14 @@ public class Graph {
 	    }
 	}
 	return nodes.get(node).getNeighbors();
-	//return new ArrayList<Node>();
     }
     
+    /**
+     * Function to get a node for the graph.
+     * @param name of the node to get.
+     * @param alias of the node to get.
+     * @return The node with the name and alias passed as parameters.
+     */
     public Node getNode(String name, String alias) {
 	int node = 0;
 	for(int i = 0; i < nodes.size(); i++) {
@@ -42,9 +63,13 @@ public class Graph {
 	    }
 	}
 	return nodes.get(node);
-	//return new ArrayList<Node>();
     }
     
+    /**
+     * Function to get one node by its name.
+     * @param name of the node to get.
+     * @return The node with the name.
+     */
     public Node getNodebyName(String name) {
     	int node = 0;
     	for(int i = 0; i < nodes.size(); i++) {
@@ -54,9 +79,13 @@ public class Graph {
     	    }
     	}
     	return nodes.get(node);
-    	//return new ArrayList<Node>();
-        }
+    }
     
+    /**
+     * Function to get one node by its alias.
+     * @param alias of the node to get.
+     * @return The node with the alias.
+     */
     public Node getNodebyAlias(String alias) {
     	int node = 0;
     	for(int i = 0; i < nodes.size(); i++) {
@@ -66,8 +95,5 @@ public class Graph {
     	    }
     	}
     	return nodes.get(node);
-    	//return new ArrayList<Node>();
-        }
-    
-    
+    }
 }
