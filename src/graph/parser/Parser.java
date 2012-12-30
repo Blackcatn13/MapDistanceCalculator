51cbaf4ed36aa4f39ca2824fe1232416c61e669a
xyz0k <adri25_sbd@hotmail.com>
2012-12-30 19:00:31 +0100

    Add quadrantInit method on Parser.java

--- src/graph/parser/Parser.java
+++ src/graph/parser/Parser.java
@@ -9,8 +9,11 @@ import graph.components.Node;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+
+import algorithm.Position;
 
 /**
  * @author xyz0k
@@ -91,5 +94,21 @@ public class Parser {
 		    graph.addNode(nodeArray.get(i));
 		}
 		return graph;
+	}
+	
+	public ArrayList<Position> quadrantInit(String filename) throws Exception{
+		FileInputStream fstream = new FileInputStream("heuristicInit/".concat(filename));
+		DataInputStream in = new DataInputStream(fstream);
+		BufferedReader file = new BufferedReader(new InputStreamReader(in));
+		ArrayList<Position> alPos = new ArrayList<Position>();
+		Node node = new Node();
+		int nNodes = Integer.parseInt(file.readLine());
+		String[] splitLine;
+		for (int i = 0; i < nNodes; i++){
+			splitLine = file.readLine().split(" ");
+			alPos.add(new Position(Integer.parseInt(splitLine[1]), Integer.parseInt(splitLine[2]), splitLine[0]));
+		}		
+		file.close();
+		return alPos;
 	}
 }
