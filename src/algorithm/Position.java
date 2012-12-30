37014b8bba8d9fe31abf64aa94659ed05bf5d461
Marc <mdd_13@hotmail.com>
2012-12-30 18:20:36 +0100

    Added new class position. Added comments to the new class.

---
+++ src/algorithm/Position.java
@@ -0,0 +1,67 @@
+/**
+ * File: Position.java
+ * Created on 30/12/2012 by Marc
+ * 
+ * Class that represents a position of a node in a 2D space
+ */
+package algorithm;
+
+public class Position {
+    /**
+     * Variable for the x coordinate.
+     */
+    private int x;
+    /**
+     * Variable for the y coordinate.
+     */
+    private int y;
+    /**
+     * Variable for the name.
+     */
+    private String name;
+    
+    /**
+     * Default Constructor.
+     */
+    public Position() {
+	x = 0;
+	y = 0;
+	name = "";
+    }
+    
+    /**
+     * Constructor with all the parameters.
+     * @param nx x position of the node.
+     * @param ny y position of the node.
+     * @param n name of the node.
+     */
+    public Position(int nx, int ny, String n) {
+	x = nx;
+	y = ny;
+	name = n;
+    }
+    
+    /**
+     * Getter of the name.
+     * @return The name of the node.
+     */
+    public String getName() {
+	return name;
+    }
+    
+    /**
+     * Getter of the x position.
+     * @return The x position.
+     */
+    public int getX() {
+	return x;
+    }
+    
+    /**
+     * Getter of the y position.
+     * @return The y position.
+     */
+    public int getY() {
+	return y;
+    }
+}
