6ad65c420ac4879d2e460ff42922ceea5a208255
xyz0k <adri25_sbd@hotmail.com>
2012-12-31 16:16:54 +0100

    Handled try/catch block inside the method

--- src/graph/parser/ParserGraph.java
+++ src/graph/parser/ParserGraph.java
@@ -9,6 +9,7 @@ import graph.components.Node;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
@@ -25,71 +26,78 @@ public class ParserGraph {
 		nNodes = 0;
 		
 	}
-	public Graph ParseTxtFile (String filename) throws Exception{
-		FileInputStream fstream = new FileInputStream("cities/".concat(filename));
-		DataInputStream in = new DataInputStream(fstream);
-		BufferedReader file = new BufferedReader(new InputStreamReader(in));
-		String line = file.readLine();
-		String[] relation;
-		int nRel = 0;
-		int nDefinedCost;
-		float busCost;
-		float subCost;
-		float walCost;
-		//float funCost;
-		ArrayList<Node> nodeArray = new ArrayList<Node>();
-		while (line != null){
-			if (line.startsWith("//")){
-				line = file.readLine();
-				continue;
-			}
-			nNodes = Integer.parseInt(line.split(" ")[0]);
-			//System.out.println(nNodes);
-			for (int i = 0; i < nNodes; i++){
-				nodeArray.add(new Node());
-			}
-			for (int i = 0; i < nNodes; i++){
-				line = file.readLine();
-				nodeArray.get(i).setName(line);
-				nodeArray.get(i).setAlias("N".concat(String.valueOf(i+1)));
-				line = file.readLine();
-				nRel = Integer.parseInt(line.split(" ")[0]);
-				for (int j = 0; j < nRel; j++){
-					busCost = -1;
-					subCost = -1;
-					walCost = -1;
-					//funCost = -1;
+	public Graph ParseTxtFile (String filename){
+		FileInputStream fstream;
+		try{
+			fstream = new FileInputStream("cities/".concat(filename));
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader file = new BufferedReader(new InputStreamReader(in));
+			String line;
+			line = file.readLine();
+			String[] relation;
+			int nRel = 0;
+			int nDefinedCost;
+			float busCost;
+			float subCost;
+			float walCost;
+			//float funCost;
+			ArrayList<Node> nodeArray = new ArrayList<Node>();
+			while (line != null){
+				if (line.startsWith("//")){
 					line = file.readLine();
-					//relation = (nodeAct, nodeDest, cost)
-					// N1 N2 3
-					relation = line.split(" ");
-					//nodeArray.get(i).addNeighbor(nodeArray.get(Integer.parseInt(relation[1].substring(1)) - 1), Float.parseFloat(relation[2]));
-					nDefinedCost = 2*Integer.parseInt(relation[2]);
-					for (int k = 0; k < nDefinedCost; k+=2){
-						if (relation[3+k].equals("B")){
-							busCost = Float.parseFloat(relation[4+k]);
-						}
-						else if (relation[3+k].equals("S")){
-							subCost = Float.parseFloat(relation[4+k]);
-						}
-						else if (relation[3+k].equals("W")){
-							walCost = Float.parseFloat(relation[4+k]);
-						}
-						else if (relation[3+k].equals("F")){
-							//funCost = Float.parseFloat(relation[4+k]);
-						}
-					}
-					//addNeighbor(String name, String alias, float Bcost, float Scost, float Wcost)
-					nodeArray.get(i).addNeighbor(nodeArray.get(Integer.parseInt(relation[1].substring(1)) - 1), busCost, subCost, walCost);
+					continue;
 				}
-				//line = file.readLine();
+				nNodes = Integer.parseInt(line.split(" ")[0]);
+				//System.out.println(nNodes);
+				for (int i = 0; i < nNodes; i++){
+					nodeArray.add(new Node());
+				}
+				for (int i = 0; i < nNodes; i++){
+					line = file.readLine();
+					nodeArray.get(i).setName(line);
+					nodeArray.get(i).setAlias("N".concat(String.valueOf(i+1)));
+					line = file.readLine();
+					nRel = Integer.parseInt(line.split(" ")[0]);
+					for (int j = 0; j < nRel; j++){
+						busCost = -1;
+						subCost = -1;
+						walCost = -1;
+						//funCost = -1;
+						line = file.readLine();
+						//relation = (nodeAct, nodeDest, cost)
+						// N1 N2 3
+						relation = line.split(" ");
+						//nodeArray.get(i).addNeighbor(nodeArray.get(Integer.parseInt(relation[1].substring(1)) - 1), Float.parseFloat(relation[2]));
+						nDefinedCost = 2*Integer.parseInt(relation[2]);
+						for (int k = 0; k < nDefinedCost; k+=2){
+							if (relation[3+k].equals("B")){
+								busCost = Float.parseFloat(relation[4+k]);
+							}
+							else if (relation[3+k].equals("S")){
+								subCost = Float.parseFloat(relation[4+k]);
+							}
+							else if (relation[3+k].equals("W")){
+								walCost = Float.parseFloat(relation[4+k]);
+							}
+							else if (relation[3+k].equals("F")){
+								//funCost = Float.parseFloat(relation[4+k]);
+							}
+						}
+						//addNeighbor(String name, String alias, float Bcost, float Scost, float Wcost)
+						nodeArray.get(i).addNeighbor(nodeArray.get(Integer.parseInt(relation[1].substring(1)) - 1), busCost, subCost, walCost);
+					}
+					//line = file.readLine();
+				}
+			line = file.readLine();
 			}
-		line = file.readLine();
+			for(int i = 0; i < nNodes; i++) {
+			    graph.addNode(nodeArray.get(i));
+			}
+			file.close();
+			return graph;
+		}catch (IOException e){
+			e.printStackTrace();
+			return null;
 		}
-		file.close();
-		for(int i = 0; i < nNodes; i++) {
-		    graph.addNode(nodeArray.get(i));
-		}
-		return graph;
 	}
 }
