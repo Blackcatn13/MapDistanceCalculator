895ae497a8511873b28ed3afe92874353a93e0dd
Marc <mdd_13@hotmail.com>
2013-01-03 19:28:37 +0100

    CheckStyle passed.

--- src/algorithm/Pair.java
+++ src/algorithm/Pair.java
@@ -1,25 +1,39 @@
+package algorithm;
+
 /**
  * File: Pair.java
  * Created on 02/01/2013 by Marc
- * 
+ *
  * Class to hold two elements.
  */
-package algorithm;
-
 public class Pair {
 
+    /**
+     * Variable that holds a cost.
+     */
     private float c;
-    
+
+    /**
+     * Variable that holds the name of the line.
+     */
     private String n;
-    
+
+    /**
+     * Default constructor of the class.
+     */
     public Pair() {
-	c = 0;
-	n = "";
+        c = 0;
+        n = "";
     }
-    
+
+    /**
+     * Constructor that takes the cost and the line name.
+     * @param nc Cost.
+     * @param nn Line name.
+     */
     public Pair(float nc, String nn) {
-	c = nc;
-	n = nn;
+        c = nc;
+        n = nn;
     }
 
     /**
@@ -49,5 +63,5 @@ public class Pair {
     public final void setN(String n) {
         this.n = n;
     }
-    
+
 }
