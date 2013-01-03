c943a2240538b703e4a83d3a398386103ac86adc
Marc <mdd_13@hotmail.com>
2013-01-03 19:39:22 +0100

    Identation fixed.

--- src/graph/components/Cost.java
+++ src/graph/components/Cost.java
@@ -19,94 +19,94 @@ public class Cost {
     private EnumMap<Transports, Float> costs = new EnumMap<Transports, Float>(Transports.class);
     private float defaultCost;
     private boolean haveSchedule = false;
-    
+
     public Cost(float Bc, float Sc, float Wc) {
-	costs.put(Transports.BUS, Bc);
-	costs.put(Transports.SUBWAY, Sc);
-	costs.put(Transports.WALK, Wc);
-	
+        costs.put(Transports.BUS, Bc);
+        costs.put(Transports.SUBWAY, Sc);
+        costs.put(Transports.WALK, Wc);
+
     }
     public Cost(boolean time) {
-	defaultCost = 5;
-	haveSchedule = time;
+        defaultCost = 5;
+        haveSchedule = time;
     }
-    
+
     public Cost(float c) {
-	defaultCost = c;
+        defaultCost = c;
     }
-    
+
     public void addCost(Date hour, float nCost) {
-	schedule.add(hour);
-	cost.add(nCost);
+        schedule.add(hour);
+        cost.add(nCost);
     }
-    
+
     public float getCost(Date hour) {
-	Iterator<Date> it = schedule.iterator();
-	int i = 0;
-	while(it.hasNext()) {
-	    if(it.next().after(hour)) {
-		return cost.get(i);
-	    }
-	    i++;
-	}
-	return defaultCost;
+        Iterator<Date> it = schedule.iterator();
+        int i = 0;
+        while(it.hasNext()) {
+            if(it.next().after(hour)) {
+                return cost.get(i);
+            }
+            i++;
+        }
+        return defaultCost;
     }
-    
+
     public boolean haveSchedule() {
-	return haveSchedule;
+        return haveSchedule;
     }
-    
+
     public float getCost() {
-	return defaultCost;
+        return defaultCost;
     }
-    
+
     public void setBusC(float c) {
-	costs.put(Transports.BUS, c);
+        costs.put(Transports.BUS, c);
     }
-    
+
     public void setSubWC(float c) {
-	costs.put(Transports.SUBWAY, c);
+        costs.put(Transports.SUBWAY, c);
     }
-    
+
     public void setWalkC(float c) {
-	costs.put(Transports.WALK, c);
+        costs.put(Transports.WALK, c);
     }
-    
+
     public float getBusC() {
-	return costs.get(Transports.BUS);
+        return costs.get(Transports.BUS);
     }
-    
+
     public float getSubWC() {
-	return costs.get(Transports.SUBWAY);
+        return costs.get(Transports.SUBWAY);
     }
-    
+
     public float getWalkC() {
-	return costs.get(Transports.WALK);
+        return costs.get(Transports.WALK);
     }
-    
+
     public Transports getMinTrans(BitSet b) {
-	float bus = (b.get(0) && costs.get(Transports.BUS) != -1) ? costs.get(Transports.BUS) : 1000;
-	float sub = (b.get(1) && costs.get(Transports.SUBWAY) != -1) ? costs.get(Transports.SUBWAY) : 1000;
-	float walk = (b.get(2) && costs.get(Transports.WALK) != -1) ? costs.get(Transports.WALK) : 1000;
-	
-	Transports t = Transports.NOTHING;
-	
-	if(bus < sub) {
-	    if(bus < walk) {
-		t = Transports.BUS;
-	    }
-	    else {
-		t = Transports.WALK;
-	    }
-	}
-	else {
-	    if(sub < walk) {
-		t = Transports.SUBWAY;
-	    }
-	    else {
-		t = Transports.WALK;
-	    }
-	}
-	return t;
+        float bus = (b.get(0) && costs.get(Transports.BUS) != -1) ? costs.get(Transports.BUS) : 1000;
+        float sub = (b.get(1) && costs.get(Transports.SUBWAY) != -1) ? costs.get(Transports.SUBWAY) : 1000;
+        float walk = (b.get(2) && costs.get(Transports.WALK) != -1) ? costs.get(Transports.WALK) : 1000;
+
+        Transports t = Transports.NOTHING;
+
+        if(bus < sub) {
+            if(bus < walk) {
+                t = Transports.BUS;
+            }
+            else {
+                t = Transports.WALK;
+            }
+        }
+        else {
+            if(sub < walk) {
+                t = Transports.SUBWAY;
+            }
+            else {
+                t = Transports.WALK;
+            }
+        }
+        return t;
     }
 }
