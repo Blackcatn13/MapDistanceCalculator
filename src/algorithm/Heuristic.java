90d87c7d551b9a940f646f24113003b08af39147
Marc <mdd_13@hotmail.com>
2013-01-03 19:19:56 +0100

    CheckStyle passed.

--- src/algorithm/Heuristic.java
+++ src/algorithm/Heuristic.java
@@ -1,37 +1,37 @@
-/**
- * File: Heuristic.java
- * Created on 26/12/2012 by Marc
- * 
- * Class that represents an abstract heuristic.
- */
 package algorithm;
 
 import graph.components.Graph;
 import graph.components.Node;
 import graph.components.Transports;
 
+/**
+ * File: Heuristic.java
+ * Created on 26/12/2012 by Marc
+ *
+ * Class that represents an abstract heuristic.
+ */
 public abstract class Heuristic {
 
     /**
      * Graph to perform the calculus.
      */
-    
+
     private Graph graph;
     /**
      * Function to initialize the heuristic.
      * @param g graph to work with.
      */
     public abstract void init(Graph g);
-    
-    
+
+
     /**
      * Function that returns the heuristic value from a node in the graph saved.
      * @param s node from where calculate the heuristic.
      * @param d node to where calculate the heuristic.
      * @return The estimated cost to got from n to d.
      */
-    public abstract float Calculate(Node s, Node d);
-    
+    public abstract float calculate(Node s, Node d);
+
     /**
      * Function that returns the heuristic value from a node in the graph saved.
      * @param s node from where calculate the heuristic.
@@ -39,14 +39,14 @@ public abstract class Heuristic {
      * @param t transport to use.
      * @return The estimated cost to got from n to d.
      */
-    public abstract float Calculate(Node s, Node d, Transports t);
+    public abstract float calculate(Node s, Node d, Transports t);
 
     /**
      * Function to get the graph.
      * @return the graph
      */
     public Graph getGraph() {
-	return graph;
+        return graph;
     }
 
     /**
@@ -54,6 +54,6 @@ public abstract class Heuristic {
      * @param graph the graph to set
      */
     public void setGraph(Graph graph) {
-	this.graph = graph;
+        this.graph = graph;
     }
 }
