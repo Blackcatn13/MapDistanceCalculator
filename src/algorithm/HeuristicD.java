ed67da1082ccd3f99bb78bc723647ef2fca44c47
Marc <mdd_13@hotmail.com>
2013-01-03 19:21:42 +0100

    CheckStyle passed. Fixed errors from CheckStyle.

--- src/algorithm/HeuristicD.java
+++ src/algorithm/HeuristicD.java
@@ -4,30 +4,30 @@ import graph.components.Graph;
 import graph.components.Node;
 import graph.components.Transports;
 
-public class HeuristicD extends Heuristic{
+public class HeuristicD extends Heuristic {
 
     /* (non-Javadoc)
      * @see algorithm.Heuristic#init(graph.components.Graph)
      */
     @Override
     public void init(Graph g) {
-	this.setGraph(g);
+        this.setGraph(g);
     }
 
     /* (non-Javadoc)
      * @see algorithm.Heuristic#Calculate(graph.components.Node, graph.components.Node)
      */
     @Override
-    public float Calculate(Node s, Node d) {
-	return 0;
+    public float calculate(Node s, Node d) {
+        return 0;
     }
 
     /* (non-Javadoc)
      * @see algorithm.Heuristic#Calculate(graph.components.Node, graph.components.Node, graph.components.Transports)
      */
     @Override
-    public float Calculate(Node s, Node d, Transports t) {
-	return 0;
+    public float calculate(Node s, Node d, Transports t) {
+        return 0;
     }
 
 }
