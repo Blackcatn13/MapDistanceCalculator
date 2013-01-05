b13fee752dc11733a37e7bc643c756a6b6b2bda8
xyz0k <adri25_sbd@hotmail.com>
2013-01-05 20:08:17 +0100

    Implemented combination of transports in outputgenerator class.

--- src/outputs/OutputGenerator.java
+++ src/outputs/OutputGenerator.java
@@ -78,103 +78,110 @@ public class OutputGenerator {
             int[][] maxTraLin = new int[][]{{2,0},{4,0},{0,1},{0,3},{0,5},{2,2},{3,5}};
             b.clear();
             for (Heuristic h : hList){
-                file.write("Heuristic usage: ");
-                        file.write(String.valueOf(h.getClass().getName()));
-                        if (h.getClass().getName().equals("algorithm.HeuristicD")){
-                                dijkstra = true;
-                                dijList = new ArrayList<Float>();
-                        }
-                        else
-                                nodijList = new ArrayList<Float>();
-                        file.newLine();
-                        file.write(dashed);
-                        file.newLine();
-                        for(int k = 0; k < init.length; k++){
-                                b.set(0,init[k][0]);
-                                b.set(1,init[k][1]);
-                                b.set(2,init[k][2]);
-                                averageTime = 0;
-                                averageNVisited = 0;
-                                actTime = 0;
-                                maxTime = 0;
-                                iter = 0;
-                                for (int i = 0; i < graph.getGraphSize(); i++){
-                                    allNodes.add("N".concat(String.valueOf(i+1)));
-                                    actNodes.add("N".concat(String.valueOf(i+1)));
-                                }
-                                for (int i = 0; i < graph.getGraphSize(); i++){
-                                    if(actNodes.size() > 0){
-                                                iniNode = actNodes.get(0);
-                                                actNodes.remove(0);
-                                                for (int j = 0; j < actNodes.size(); j++){
-                                                    astar = new AStar(graph,graph.getNodebyAlias(iniNode), graph.getNodebyAlias(actNodes.get(j)), h, maxTransfers, maxLines);
-                                                    time = new Time();
-                                                    time.setScale("millisecond");
-                                                    nodes = astar.getPath(b);
-                                                    if (dijkstra && nodes.size()>=2){
-                                                        dijList.add(nodes.get(nodes.size()-2).getCost());
-                                                    }
-                                                    if(!dijkstra && nodes.size()>=2){
-                                                    	Float eff  = (dijList.get(j)/nodes.get(nodes.size()-2).getCost())*100;
-                                                        System.out.println("D cost: " + dijList.get(j) + " ND cost: " + nodes.get(nodes.size()-2).getCost() + " %Eff: " + eff);
-                                                    	nodijList.add(eff);
-                                                    }
-                                                    actTime = time.elapsedTime();
-                                                    if (maxTime < actTime){
-                                                        maxTime = actTime;
-                                                    }
-                                                    if (finMaxTime < maxTime){
-                                                        finMaxTime = maxTime;
-                                                        maxNodePath = nodes;
-                                                        maxPathVisited = astar.getVisitedNodes();
-                                                        if (!nodes.isEmpty())
-                                                                maxCost = nodes.get(nodes.size() - 2).getCost();
-                                                    }
-                                                    averageTime += actTime;
-                                                    averageNVisited += astar.getVisitedNodes();
-                                                    iter++;
-                                                }
-                                    }
-                                }
-                                finAverageTime += averageTime/iter;
-                                finAverageNVisited += averageNVisited/iter;
-                                finIter++;
-                        }
-                            file.write("Average calculate path time: " + String.valueOf(finAverageTime/finIter));
-                            file.write(" average num nodes visited: " + String.valueOf((int) finAverageNVisited/finIter));
-                            file.newLine();
-                            if (dijkstra) file.write("Dijkstra heuristic has 100% effectivity");
-                            else{
-                                int averageCost = 0;
-                                int counter = 0;
-                                for (Float cost : nodijList){
-                                	averageCost += cost;
-                                    counter++;
-                                }
-                                file.write("This heuristic has an average of: " + averageCost/counter + "% effectivity");
+                for(int ii = 0; ii< maxTraLin.length; ii++){
+            		maxTransfers = maxTraLin[ii][0];
+            		maxLines = maxTraLin[ii][1];
+                	file.write("Heuristic usage: ");
+                    file.write(String.valueOf(h.getClass().getName()));
+                    if (h.getClass().getName().equals("algorithm.HeuristicD")){
+                            dijkstra = true;
+                            dijList = new ArrayList<Float>();
+                    }
+                    else
+                            nodijList = new ArrayList<Float>();
+                    file.newLine();
+                    file.write(dashed);
+                    file.newLine();
+                    for(int k = 0; k < init.length; k++){
+                            b.set(0,init[k][0]);
+                            b.set(1,init[k][1]);
+                            b.set(2,init[k][2]);
+                            averageTime = 0;
+                            averageNVisited = 0;
+                            actTime = 0;
+                            maxTime = 0;
+                            iter = 0;
+                            for (int i = 0; i < graph.getGraphSize(); i++){
+                                allNodes.add("N".concat(String.valueOf(i+1)));
+                                actNodes.add("N".concat(String.valueOf(i+1)));
                             }
-                            file.newLine();
-                            file.write("Max calculate path: " + String.valueOf(finMaxTime));
-                            file.write(" num nodes visited: " + String.valueOf(maxPathVisited));
-                            file.newLine();
-                            file.write("Path of max details");
-                            file.newLine();
-                                    file.write("\t" + "Node start: " + maxNodePath.get(0).getSNode().getAlias());
-                                    file.newLine();
-                                    file.write("\t" + "Node end: " + maxNodePath.get(maxNodePath.size()-1).getSNode().getAlias());
-                                    file.newLine();
-                                    for (int i = 0; i < maxNodePath.size()-1; i++){
-                                        InfoPath ip = maxNodePath.get(i);
-                                        file.write("\t" + ip.getSNode().getName()+ " ("+ip.getSNode().getAlias() + ")" + " to " + maxNodePath.get(i+1).getSNode().getName()+ " (" + maxNodePath.get(i+1).getSNode().getAlias()+ ")");
-                                        file.newLine();
-                                    }
-                                    file.write("\tTotal cost: " + maxCost);
+                            for (int i = 0; i < graph.getGraphSize(); i++){
+                                if(actNodes.size() > 0){
+                                            iniNode = actNodes.get(0);
+                                            actNodes.remove(0);
+                                            for (int j = 0; j < actNodes.size(); j++){
+                                                astar = new AStar(graph,graph.getNodebyAlias(iniNode), graph.getNodebyAlias(actNodes.get(j)), h, maxTransfers, maxLines);
+                                                time = new Time();
+                                                time.setScale("millisecond");
+                                                nodes = astar.getPath(b);
+                                                if (dijkstra && nodes.size()>=2){
+                                                    dijList.add(nodes.get(nodes.size()-2).getCost());
+                                                }
+                                                if(!dijkstra && nodes.size()>=2){
+                                                	if (iter==58){
+                                                		dijkstra = false;
+                                                	}
+                                                	Float eff  = (dijList.get(j)/nodes.get(nodes.size()-2).getCost())*100;
+                                                    System.out.println("D cost: " + dijList.get(j) + " ND cost: " + nodes.get(nodes.size()-2).getCost() + " %Eff: " + eff);
+                                                	nodijList.add(eff);
+                                                }
+                                                actTime = time.elapsedTime();
+                                                if (maxTime < actTime){
+                                                    maxTime = actTime;
+                                                }
+                                                if (finMaxTime < maxTime){
+                                                    finMaxTime = maxTime;
+                                                    maxNodePath = nodes;
+                                                    maxPathVisited = astar.getVisitedNodes();
+                                                    if (!nodes.isEmpty())
+                                                            maxCost = nodes.get(nodes.size() - 2).getCost();
+                                                }
+                                                averageTime += actTime;
+                                                averageNVisited += astar.getVisitedNodes();
+                                                iter++;
+                                            }
+                                }
+                            }
+                            finAverageTime += averageTime/iter;
+                            finAverageNVisited += averageNVisited/iter;
+                            finIter++;
+                    }
+                        file.write("Average calculate path time: " + String.valueOf(finAverageTime/finIter));
+                        file.write(" average num nodes visited: " + String.valueOf((int) finAverageNVisited/finIter));
                         file.newLine();
-                        file.newLine();
-                        if (!dijkstra)
-                        	nodijList.clear();
-                        else
-                        	dijkstra = false;
+                }
+                if (dijkstra) file.write("Dijkstra heuristic has 100% effectivity");
+                else{
+                    int averageCost = 0;
+                    int counter = 0;
+                    for (Float cost : nodijList){
+                    	averageCost += cost;
+                        counter++;
+                    }
+                    file.write("This heuristic has an average of: " + averageCost/counter + "% effectivity");
+                }
+                file.newLine();
+                file.write("Max calculate path: " + String.valueOf(finMaxTime));
+                file.write(" num nodes visited: " + String.valueOf(maxPathVisited));
+                file.newLine();
+                file.write("Path of max details");
+                file.newLine();
+                file.write("\t" + "Node start: " + maxNodePath.get(0).getSNode().getAlias());
+                file.newLine();
+                file.write("\t" + "Node end: " + maxNodePath.get(maxNodePath.size()-1).getSNode().getAlias());
+                file.newLine();
+                for (int i = 0; i < maxNodePath.size()-1; i++){
+                    InfoPath ip = maxNodePath.get(i);
+                    file.write("\t" + ip.getSNode().getName()+ " ("+ip.getSNode().getAlias() + ")" + " to " + maxNodePath.get(i+1).getSNode().getName()+ " (" + maxNodePath.get(i+1).getSNode().getAlias()+ ")");
+                    file.newLine();
+                }
+                file.write("\tTotal cost: " + maxCost);
+	            file.newLine();
+	            file.newLine();
+	            if (!dijkstra)
+	            	nodijList.clear();
+	            else
+	            	dijkstra = false;
             }
             file.close();
         }catch (IOException e) {
@@ -204,7 +211,7 @@ public class OutputGenerator {
         h.init(graph);
         h.setParams(names);
         hList.add(h);
-        /*h = new HeuristicL();
+        h = new HeuristicL();
         names.clear();
         names.add("S1.txt");
         names.add("S3.txt");
@@ -215,7 +222,7 @@ public class OutputGenerator {
         names.add("B5.txt");
         names.add("B6.txt");
         h.init(graph);
-        hList.add(h);*/
+        hList.add(h);
         oG.generateTXT(city.concat("output.txt"), graph, hList);        
     }
     
